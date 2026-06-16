f53a33b6f49351f76b578f7ba20cfaf2375a6609
arsh <arshia.hy.coding@gmail.com>
2026-06-16 12:18:48 +0330

    end Day 001

--- Day 001/test.py
+++ Day 001/test.py
@@ -1,4 +1,18 @@
+print("Welcome to the bandname generator")
+
 city_name = input("What's the name of the city you grew up in?\n")
 pet_name = input("What's your pet's name?\n")
 
 print(f"Your band name could be {city_name} {pet_name}")
+
+# # len string
+# username = input("What is your name?\n")
+# print(f"your name is {username} and len name : {len(username)}")
+
+# # len string 2
+# username = input("What is your name?\n")
+# lenght = len(username)
+# print(lenght)
+
+# # len string 3
+# print(len(input("What is your name?\n")))
