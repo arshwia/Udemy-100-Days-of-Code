830c2f186245cf99007738f52e91536a92764d80
arsh <arshia.hy.coding@gmail.com>
2026-06-17 12:51:53 +0330

    {

---
+++ Day 002/BMI.py
@@ -0,0 +1,6 @@
+height = float(input("your height?(M 1.80)\n"))
+weight = float(input("your weight?(K 80)\n"))
+
+bmi = weight / height**2
+
+print(f"height = {height}\nweight = {weight}\n\n\nyour BMI is {bmi}")
