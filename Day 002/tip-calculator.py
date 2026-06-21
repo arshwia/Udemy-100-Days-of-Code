1b1b6c086c1dcad61598edc65a17be9093b55e4e
arsh <arshia.hy.coding@gmail.com>
2026-06-21 19:50:39 +0330

    refactor: rename variable
    invoice to bill
    all_mony to bill_with_tip

--- Day 002/tip-calculator.py
+++ Day 002/tip-calculator.py
@@ -1,12 +1,12 @@
 print("Welcome to the tip calculator!")
 
-invoice = float(input("What was total bill? $"))
+bill = float(input("What was total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
 people = int(input("How many people to split the bill? "))
 
-tip_mony = invoice * tip / 100
-all_mony = invoice + tip_mony
+tip_mony = bill * tip / 100
+bill_with_tip = bill + tip_mony
 
-each_person = all_mony / people
+each_person = bill_with_tip / people
 
 print(f"Each person should pay: ${round(each_person, 2)}")
