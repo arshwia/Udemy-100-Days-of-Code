18c202436253c164f753573d87bd2f0ef378a6d2
arsh <arshia.hy.coding@gmail.com>
2026-06-22 19:24:20 +0330

    feat: add nested if practice

---
+++ Day 003/can you work.py
@@ -0,0 +1,12 @@
+did_you_eat = True
+drink_coffee = False
+
+if did_you_eat:
+    if drink_coffee:
+        print("You can work!")
+    else:
+        print("You can't work!")
+        print("Because you didn't drink coffee.")
+else:
+    print("You can't work!")
+    print("Because you didn't eat.")
