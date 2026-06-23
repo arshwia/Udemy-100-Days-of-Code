6124cc8f96b9dd09d64d6ed987a22e13795d5010
arsh <arshia.hy.coding@gmail.com>
2026-06-23 18:19:25 +0330

    feat: accept user input for work condition

--- Day 003/can you work.py
+++ Day 003/can you work.py
@@ -1,8 +1,9 @@
-did_you_eat = True
-drink_coffee = False
+did_you_eat = input("did you eat? (True of False)")
 
-if did_you_eat:
-    if drink_coffee:
+if did_you_eat == "True":
+    drink_coffee = input("did you coffee? (True of False)")
+
+    if drink_coffee == "True":
         print("You can work!")
     else:
         print("You can't work!")
