33e2e2c8575dd7264cb41181316902f3f5890e29
arsh <arshia.hy.coding@gmail.com>
2026-06-23 19:30:48 +0330

    feat: add photo option to ticket pricing

--- Day 003/height.py
+++ Day 003/height.py
@@ -1,10 +1,29 @@
 height = int(input("How tall are you(cm)? "))
+price = 0
 
 if height >= 120:
     age = int(input("pls enter your age: "))
     if age <= 18:
-        print("You can ride it and it costs $7.")
+        price = 7
+        print(f"You can ride it and it costs ${price}.")
+
+        photo = input("Do you want a photo, $3 will be added to the price.(Yes or no)?")
+
+        if photo == "Yes":
+            price = price + 3
+            print(f"You can ride it and it costs ${price}")
+        else:
+            print(f"You can ride it and it costs ${price}")
     else:
-        print("You can ride it and it costs $18.")
+        price = 18
+        print(f"You can ride it and it costs ${price}.")
+
+        photo = input("Do you want a photo, $3 will be added to the price.(Yes or no)?")
+
+        if photo == "Yes":
+            price = price + 3
+            print(f"You can ride it and it costs ${price}")
+        else:
+            print(f"You can ride it and it costs ${price}")
 else:
     print("I'm sorry, you can't ride.")
