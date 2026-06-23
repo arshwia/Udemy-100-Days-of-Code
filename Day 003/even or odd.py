89a495d265bc3500d7ca198bfa09f22accf25766
arsh <arshia.hy.coding@gmail.com>
2026-06-23 18:42:15 +0330

    feat: add even or odd.py file

---
+++ Day 003/even or odd.py
@@ -0,0 +1,7 @@
+print("Even or odd?")
+number = int(input("enter a number (int number) :"))
+
+if number % 2 == 0:
+    print(f"your number is Even -> {number}")
+else:
+    print(f"your number is odd -> {number}")
