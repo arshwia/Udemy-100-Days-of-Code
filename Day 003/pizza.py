4126bb5e26da29871cfb7f39e4f78d2397a63dfa
arsh <arshia.hy.coding@gmail.com>
2026-06-24 19:28:01 +0330

    refactor: simplify pizza pricing logic
    ma in kar ro kardim k az tkrar code jlogiri konim

--- Day 003/pizza.py
+++ Day 003/pizza.py
@@ -1,45 +1,24 @@
-print("Welcome to Python pizza")
-
 size = input("What size pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y, N: ")
 
-price = 0
-
-# small pizza (S) = $15
+bill = 0
 if size == "S":
-    price = price + 15
-
-    pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-    if pepperoni == "Y":
-        price = price + 2
-
-    extra_cheese = input("Do you want extra cheese? Y, N: ")
-    if extra_cheese == "Y":
-        price = price + 1
-
-    print(f"your pizza price : ${price}")
-
+    bill += 15
 elif size == "M":
-    price = price + 20
-
-    pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-    if pepperoni == "Y":
-        price = price + 3
-
-    extra_cheese = input("Do you want extra cheese? Y, N: ")
-    if extra_cheese == "Y":
-        price = price + 1
-
-    print(f"your pizza price : ${price}")
-
+    bill += 20
 elif size == "L":
-    price = price + 25
+    bill += 25
+else:
+    print("You typed the wrong inputs.")
 
-    pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-    if pepperoni == "Y":
-        price = price + 3
+if pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
 
-    extra_cheese = input("Do you want extra cheese? Y, N: ")
-    if extra_cheese == "Y":
-        price = price + 1
+if extra_cheese == "Y":
+    bill += 1
 
-    print(f"your pizza price : ${price}")
+print(f"Your final bill is {bill}")
