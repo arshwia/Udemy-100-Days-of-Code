3ba1bc1f050a61f04f1bef373ff0471f0ab56d3c
arsh <arshia.hy.coding@gmail.com>
2026-07-06 18:27:26 +0330

    feat: implement Hangman game logic
    - add main game loop
    - reveal correctly guessed letters
    - track guessed letters
    - decrease lives on wrong guesses
    - detect win and lose conditions

--- Day 007/main.py
+++ Day 007/main.py
@@ -34,6 +34,15 @@ words = [
     "planet",
 ]
 
+print(""" _
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _' | '_ \ / _' | '_ ' _ \ / _' | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/""")
+
 
 # functions
 def choose_word(words):
@@ -43,25 +52,55 @@ def choose_word(words):
     return random_word
 
 
-def hidden_word_def():
+def create_hidden_word():
+    """Random word selection and word hiding"""
     word = choose_word(words)
     word_length = len(word)
 
-    hidden_word = ""
+    hidden_word = []
     for _ in range(word_length):
-        hidden_word += "_ "
+        hidden_word.append("_")
 
-    print(word)
-    print(hidden_word)
-    return hidden_word
+    word_and_hidden_word = [word, hidden_word]
 
-
-def play_game():
-    hidden_word = hidden_word_def()
+    return word_and_hidden_word
 
 
-def display_hangman():
-    print()
+lives = 6
 
 
-play_game()
+def play_game(lives):
+    word, hidden_word = create_hidden_word()
+    print(f"Word to guess: {' '.join(hidden_word)}")
+
+    guessed_letters = []
+
+    while lives > 0 and "_" in hidden_word:
+        user_letter = input("Guess a letter: ").lower()
+
+        if user_letter in guessed_letters:
+            print("You already guessed that letter.")
+            continue
+
+        guessed_letters.append(user_letter)
+
+        found = False
+
+        for index, letter in enumerate(word):
+            if user_letter == letter:
+                hidden_word[index] = letter
+                found = True
+
+        if not found:
+            lives -= 1
+            print(f"Wrong! Lives left: {lives}")
+
+        print(f"word to guess: {' '.join(hidden_word)}")
+
+    if "_" not in hidden_word:
+        print(f"win\n\tThe word was: {word}")
+    else:
+        print(f"lose\n\tThe word was: {word}")
+
+
+play_game(lives)
