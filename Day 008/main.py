27816068e8539f120db4452cd71c9f8295fc0725
arsh <arshia.hy.coding@gmail.com>
2026-07-09 15:26:00 +0330

    feat: add encode(), remove split_msg()

--- Day 008/main.py
+++ Day 008/main.py
@@ -1,57 +1,69 @@
 from art_caesar_cipher import logo
 from data_caesar_cipher import valid_characters
 
-# print(logo)
-# encode_or_decode = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n\t")
-encode_or_decode = "encode"
+print(logo)
 
+encode_or_decode = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n\t")
 msg = str(input("Type your massage: \n\t").lower())
 shift_number = int(input("Type the shift number: \n\t"))
 
 
-def split_msg(msg):
-    msg_list = msg.split(" ")
+def check_data(msg):
+    """بررسی می‌کند که همه کاراکترهای پیام معتبر باشند."""
 
-    return msg_list
-
-
-def encode():
-    print("encoding")
-    msg_list = split_msg(msg)
-
-    msg_encode = msg_list
-    return msg_encode
-
-
-def main(encode_or_decode, msg, shift_num):
-    if encode_or_decode == "encode":
-        print("encode")
-
-        msg_encode = encode()
-
-        print(f"Here's the encoded result: {msg_encode}")
-
-    elif encode_or_decode == "decode":
-        print("decode")
-
-    else:
-        print("your input not 'encode' or 'decode'")
-
-
-def check_data():
     for letter in msg:
         is_found = False
 
         for valid in valid_characters:
             if valid == letter:
-                print(f"valid {valid, letter}")
                 is_found = True
                 break
 
         if not is_found:
             print(f"{letter} is invalid")
+            return False
 
-    main(encode_or_decode, msg, shift_number)
+    return True
 
 
-check_data()
+def encode(msg, shift_number):
+    """اینجا بعداً الگوریتم رمزنگاری را کامل می‌کنیم."""
+
+    encoded = ""
+
+    for letter in msg:
+        if letter == " ":
+            encoded += " "
+            continue
+
+        for index, l in enumerate(valid_characters):
+            if letter == l:
+                new_index = (index + shift_number) % 26
+                encoded += valid_characters[new_index]
+
+    return encoded
+
+
+def decode(msg, shift_number):
+    """بعداً کامل می‌شود."""
+    pass
+
+
+def main(encode_or_decode, msg, shift_number):
+
+    if not check_data(msg):
+        return
+
+    if encode_or_decode == "encode":
+        msg_encode = encode(msg, shift_number)
+        print(f"Here's the encoded result: {msg_encode}")
+
+    elif encode_or_decode == "decode":
+        msg_decode = decode(msg, shift_number)
+        print(f"Here's the decoded result: {msg_decode}")
+
+    else:
+        print("Your input must be 'encode' or 'decode'")
+
+
+main(encode_or_decode, msg, shift_number)
